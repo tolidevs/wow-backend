79220bbab645471f85e740f4065bd4e8d8f768c1
flatironschool <t.benson@live.co.uk>
2020-03-10 19:44:59 +0000

    set up user create and token creation in back end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,47 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
 
+    # secret from env file
     def secret
         ENV["MY_SECRET"]
+    end
+
+    # generate a token
+    def generate_token(data)
+        JWT.encode(data, secret)
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    # take the token from the request headers, if there is a token decode it, otherwise return nil
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            puts token
+            begin
+                JWT.decode(token, secret, true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    # find furrent user by ID from decoded token
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: id)
+        end
+    end
+
+    # is there a current user logged in?
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,5 @@ class User < ApplicationRecord
     has_many :saved_shows
     has_secure_password
     validates :email, uniqueness: { case_sensitive: false }
-    validates :name, :email { presence: true }
-
-    skip_before_action :authorized, only: [:create]
-
-
-    
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-        render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-        render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
-    end
- 
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :email, :password)
-    end
+    validates :name, :email, presence: true 
 end
