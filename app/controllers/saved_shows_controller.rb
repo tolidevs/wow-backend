7d06704658f5dd43c38a76d8e8f3771817e5eb8a
flatironschool <t.benson@live.co.uk>
2020-03-23 16:43:09 +0000

    fixed bug with savign show_type

--- app/controllers/saved_shows_controller.rb
+++ app/controllers/saved_shows_controller.rb
@@ -2,7 +2,7 @@ class SavedShowsController < ApplicationController
 
     def create
         if !SavedShow.all.find{|show| show.user_id == params[:user] && show.imdbID == params[:imdbID]}
-            @saved_show = SavedShow.create(user_id: params[:user], imdbID: params[:imdbID], title: params[:title], show_type: params[:type], year: params[:year], poster: params[:poster])
+            @saved_show = SavedShow.create(user_id: params[:user], imdbID: params[:imdbID], title: params[:title], show_type: params[:show_type], year: params[:year], poster: params[:poster])
             render json: @saved_show
         elsif SavedShow.all.find{|show| show.user_id == params[:user] && show.imdbID == params[:imdbID]}
             render json: {message: "already saved"}
--- app/serializers/saved_show_serializer.rb
+++
@@ -1,3 +0,0 @@
-class SavedShowSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :imdbID, :title, :show_type, :year, :poster
-end
