9a7e33deeecf14ca848627c9276463b8480f0c7e
flatironschool <t.benson@live.co.uk>
2020-06-09 21:23:42 +0100

    fixed broken API link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins 'https://whats-on-where.netlify.app', 'https://whats-on-where.netlify.com'
+    origins 'https://whats-on-where.netlify.app', 'https://whats-on-where.netlify.com', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
